e21dcf8e960a4c30b3e861f6ee0e1d054be66b3e
justmiles <1012226+justmiles@users.noreply.github.com>
2026-02-24 15:34:34 +0000

    fix space in update-user

--- s6-overlay/s6-rc.d/update-user/scripts/update-user.sh
+++ s6-overlay/s6-rc.d/update-user/scripts/update-user.sh
@@ -6,7 +6,7 @@ set -e
 [ ! -z "${OPENCLAW_GID}" ] && groupmod -g $OPENCLAW_GID openclaw > /dev/null
 
 # if not in `~/.bashrc`, add PATH
-OPENCLAW_PATH=  "/home/openclaw/bin:/home/openclaw/.local/bin:/home/openclaw/.nix-profile/bin"
+OPENCLAW_PATH="/home/openclaw/bin:/home/openclaw/.local/bin:/home/openclaw/.nix-profile/bin"
 if ! grep -q "$OPENCLAW_PATH" "/home/openclaw/.bashrc"; then
     echo "export PATH=$PATH:$OPENCLAW_PATH" >> /home/openclaw/.bashrc
 fi
